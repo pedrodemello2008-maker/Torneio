d4d481ee1bdb5772cc82fa01332a9e9e4400ead9
pedrodemello2008-maker <pedrodemello2008@gmail.com>
2026-09-25 15:03:01 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,10 +3,11 @@
 // Veja o README.md para o passo a passo completo.
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDIQoUN-4fNA9p-x8u1Iqix8r-fiBsU",
+  apiKey: "AIzaSyCDIQoUN-4fNA9p-x-x8u1Iqix8r-fiBsU",
   authDomain: "torneio-robotica.firebaseapp.com",
   projectId: "torneio-robotica",
   storageBucket: "torneio-robotica.firebasestorage.app",
   messagingSenderId: "523431995851",
-  appId: "1:523431995851:web:4add844a33a1b6d683ee4e"
+  appId: "1:523431995851:web:4add844a33a1b6d683ee4e",
+  measurementId: "G-WJXVTPXXFB"
 };
